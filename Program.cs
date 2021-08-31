c76c78e8cf0389b2e8b4860c28fa3961dfa0240f
jotheeswaran <jotheeswaran03@outlook.com>
2021-08-31 18:03:19 +0530

    Uploadin blob in Container

--- Program.cs
+++ Program.cs
@@ -7,12 +7,21 @@ namespace Working_with_blobs
     {
         private static string _container_name = "dataone";
         private static string _connection_string = "DefaultEndpointsProtocol=https;AccountName=demostorageaccount1001;AccountKey=Zk345gT01Z+Kh+ZM4JejapnuYenCiJdOWA/fGvGiOUPO1lW1wq2m7kIbVTzng/KHm8mi33VYLGF/cG/7dqjymw==;EndpointSuffix=core.windows.net";
-
+        private static string _blog_name = "values.png";
+        private static string _blog_location = "/Users/jotheeswaran.gthoughtworks.com/Downloads/values.png";
 
         static void Main(string[] args)
         {
             BlobServiceClient blobServiceClient = new BlobServiceClient(_connection_string);
-            blobServiceClient.CreateBlobContainer(_container_name);
+            // creating container
+            // blobServiceClient.CreateBlobContainer(_container_name);
+
+            // getting container name
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(_container_name);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(_blog_name);
+            blobClient.Upload(_blog_location);
+            Console.WriteLine("Blob has been uploaded");
+
             Console.ReadKey();
         }
     }
